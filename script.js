d51546a01dbbb6831d6839ede067a6c7faa9db3d
Adam Toczko <adamtoczko80@gmail.com>
2019-11-09 16:15:28 +0100

    FirstFirst created basic to do list Created basic to do list no styles All working apart from adding ?? Now changing it for server list

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let modal;
 let modalClose; 
 let modalClose2;
 let editList;
-let modalInput;
 let mainInput;
 let addItem;
-let lastID = 0;
+let form;
+let currentId = 0;
+let currentItem;
+
 
 function main() {
   prepareDOMElements();
   prepareDOMEvents();
-  prepareInitialList();
+  // prepareInitialList(); nie potrzebne na serwerze
+  getTodosFromServer();
 }
 
 function prepareDOMElements() {
@@ -23,17 +26,33 @@ function prepareDOMElements() {
   modalClose = document.querySelector(".close"); 
   modalClose2 = document.querySelector("#cancelTodo");
   editList = document.querySelector("#acceptTodo");
-  modalInput = document.getElementById('popupInput');
   mainInput = document.getElementById('myInput');
-  addItem = document.getElementById('addTodo');
+  addItem = document.getElementById('addTodo'); // do serwera wylaczyc
+  form = document.getElementById('addForm')
 }
 
 function prepareDOMEvents() {
   // Przygotowanie listenerów
   list.addEventListener('click', listClickManager);
   addItem.addEventListener('click', addNewElementToList);
-
+  form.addEventListener('submit', addNewItemViaForm);
+  modalClose.addEventListener('click', modalClosed);
+  modalClose2.addEventListener('click', modalClosed2);
+  editList.addEventListener('click', editAccept);
 }
+
+function addNewItemViaForm (e) {
+  e.preventDefault();
+  addNewTodo();
+}
+
+function addNewTodo() {
+  if (mainInput.value.trim() !== ''){
+    addNewElementToList(mainInput.value);
+    mainInput.value = '';
+  }
+}
+
 
 function prepareInitialList() {
   // Tutaj utworzymy sobie początkowe todosy. Mogą pochodzić np. z tablicy
@@ -41,6 +60,16 @@ function prepareInitialList() {
     addNewElementToList(todo);
   });
 }
+
+function getTodosFromServer () {
+  axios.get('http://195.181.210.249:3000/todo/')
+  .then(function (response) {
+    response.data.forEach(function(todo) {
+      addNewElementToList(todo.title, todo.id);
+    })
+  })
+}
+
 
 function addNewElementToList(title   /* Title, author, id */) {
   //obsługa dodawanie elementów do listy
@@ -51,78 +80,82 @@ function addNewElementToList(title   /* Title, author, id */) {
 
 function createElement(title /* Title, author, id */) {
   // Tworzyc reprezentacje DOM elementu return newElement
-  // return newElement
-  const newElement = document.createElement('li');
-  newElement.id = 'todo-' + (++lastID);
+  currentId++;
 
+  const newElement = document.createElement('li');
+  newElement.dataset.id = currentId;
   const titleElement = document.createElement('span');
   titleElement.innerText = title;
   newElement.appendChild(titleElement);
-
-
-  // const newButton = document.createElement('button'); // tworzymy nowy button
-  // newButton.innerHTML = 'edytuj'; // dodajemy napis w buttonie 'edytuj'
-  // newButton.classList.add('edit')
-  // newElement.appendChild(newButton);  // dodajemy przycisk do naszej komórki   
   
-  // const newButton2 = document.createElement('button'); // tworzymy nowy button
-  // newButton2.innerHTML = 'usuń'; // dodajemy napis w buttonie 'delete'
-  // newButton2.classList.add('delete')
-  // newElement.appendChild(newButton2);  // dodajemy przycisk do naszej komórki
+
+  const newButton = document.createElement('button'); // tworzymy nowy button
+  newButton.innerHTML = 'edytuj'; // dodajemy napis w buttonie 'edytuj'
+  newButton.classList.add('edit')
+  newElement.appendChild(newButton);  // dodajemy przycisk do naszej komórki   
   
-  // const newButton3 = document.createElement('button'); // tworzymy nowy button
-  // newButton3.innerHTML = 'wykonane'; // dodajemy napis w buttonie 'wykonane'
-  // newButton3.classList.add('done')
-  // newElement.appendChild(newButton3);  // dodajemy przycisk do naszej komórki  
+  const newButton2 = document.createElement('button'); // tworzymy nowy button
+  newButton2.innerHTML = 'usuń'; // dodajemy napis w buttonie 'delete'
+  newButton2.classList.add('delete')
+  newElement.appendChild(newButton2);  // dodajemy przycisk do naszej komórki
+  
+  const newButton3 = document.createElement('button'); // tworzymy nowy button
+  newButton3.innerHTML = 'wykonane'; // dodajemy napis w buttonie 'wykonane'
+  newButton3.classList.add('done')
+  newElement.appendChild(newButton3);  // dodajemy przycisk do naszej komórki  
 
     return newElement;
 }
 
 
-
 function listClickManager(event) {
   // Rozstrzygnięcie co dokładnie zostało kliknięte i wywołanie odpowiedniej funkcji
  
-    let id = event.target.parentElement.id;
-
+  currentItem = event.target.parentElement.dataset.id;
   if (event.target.className === 'delete') {
-      removeListElement(id);
+      removeListElement();
   } else if (event.target.className === 'edit'){
-      let title = document.querySelector('#' + id).querySelector('span')
-      editListElement(id, title);
+      editListElement();
   } else if (event.target.className === 'done') {
-    titleElement(id).classList.add('doneItem')
+      markAsDone();
   }
-  
 }
 
-function removeListElement(id) {
-    let liElement = document.querySelector('#' + id);
-    list.removeChild(liElement);
+function removeListElement() {
+  const line = document.querySelector('li[data-id="' + currentItem + '"]');
+  list.removeChild(line);
 }
 
-function editListElement(id, title) {
-
+function editListElement() {
+  openPopup();
+  text = document.querySelector('li[data-id="' + currentItem + '"] span').textContent;
+  popupInput.value = text;
 }
 
-
-
-
-//TRZEBA DODAC OD EDYTUJ 
 function openPopup() {
     modal.style.display = "block";
 }
 
-// modalClose.onclick = function() {
-//     modal.style.display = "none";
-//   }
+function modalClosed() {
+  modal.style.display = "none";
+}
 
-// modalClose2.onclick = function() {
-//     modal.style.display = "none";
-//   }
-
-
+function modalClosed2() {
+  modal.style.display = "none";
+}
 
 
+function editAccept () {
+  const line2 = document.querySelector('li[data-id="' + currentItem + '"] span');
+  
+  line2.innerText = popupInput.value;
+  modalClosed();
+}
+
+function markAsDone() {
+  const line3 = document.querySelector('li[data-id="' + currentItem + '"] span');
+  line3.classList.add('listCompleted');
+ 
+}
 
 document.addEventListener('DOMContentLoaded', main);
